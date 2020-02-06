3e156f9da630080409d8c5d740f7822462885eb6
Dioh <dioh.desush@icloud.com>
2020-02-06 10:25:13 -0500

    Initial Commit

---
+++ Homework 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework 2
+//
+//  Created by Dioh Desuah on 2/6/20.
+//  Copyright © 2020 Dioh Desuah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
